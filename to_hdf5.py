b7ff850cbdfbc5ec68a2b31f1987a5b6237848c8
Jeremy Malloch <jeremalloch@gmail.com>
2017-12-04 08:15:59 -0500

    Script to save as hdf5 works

--- to_hdf5.py
+++ to_hdf5.py
@@ -10,7 +10,8 @@ import random
 # Make size slightly larger than the crop size in order to add a little
 # more variation
 SIZE = (256,256)
-TEST_RUN = True
+TEST_RUN = False
+DATA_COMPRESSION = False
 
 # TODO: Remove this
 # Determined from running over entire unlabed COCO 2017 image dataset overnight
@@ -39,21 +40,29 @@ training_mean_old = np.zeros((*SIZE,3), dtype=np.float32)
 training_variance = np.zeros((*SIZE,3), dtype=np.float32)
 
 # Create the HDF5 output file
-hdf5_output = h5py.File(directory + "COCO_2017_unlabeled.hdf5", mode='w')
-hdf5_output.create_dataset("train_img", (len(files_dict["train_img"]), *SIZE, 3), np.uint8, compression="gzip")
-hdf5_output.create_dataset("val_img", (len(files_dict["val_img"]), *SIZE, 3), np.uint8, compression="gzip")
-hdf5_output.create_dataset("test_img", (len(files_dict["test_img"]), *SIZE, 3), np.uint8, compression="gzip")
-hdf5_output.create_dataset("train_mean", (*SIZE, 3), np.float32, compression="gzip")
-hdf5_output.create_dataset("train_std", (*SIZE, 3), np.float32, compression="gzip")
+if DATA_COMPRESSION:
+    hdf5_output = h5py.File(directory + "COCO_2017_unlabeled_compressed.hdf5", mode='w')
+    hdf5_output.create_dataset("train_img", (len(files_dict["train_img"]), *SIZE, 3), np.uint8, compression="gzip")
+    hdf5_output.create_dataset("val_img", (len(files_dict["val_img"]), *SIZE, 3), np.uint8, compression="gzip")
+    hdf5_output.create_dataset("test_img", (len(files_dict["test_img"]), *SIZE, 3), np.uint8, compression="gzip")
+    hdf5_output.create_dataset("train_mean", (*SIZE, 3), np.float32, compression="gzip")
+    hdf5_output.create_dataset("train_std", (*SIZE, 3), np.float32, compression="gzip")
+else:
+    hdf5_output = h5py.File(directory + "COCO_2017_unlabeled_uncompressed.hdf5", mode='w')
+    hdf5_output.create_dataset("train_img", (len(files_dict["train_img"]), *SIZE, 3), np.uint8)
+    hdf5_output.create_dataset("val_img", (len(files_dict["val_img"]), *SIZE, 3), np.uint8)
+    hdf5_output.create_dataset("test_img", (len(files_dict["test_img"]), *SIZE, 3), np.uint8)
+    hdf5_output.create_dataset("train_mean", (*SIZE, 3), np.float32)
+    hdf5_output.create_dataset("train_std", (*SIZE, 3), np.float32)
 
 start_time = time()
+small_start = start_time
 for img_type, img_list in files_dict.items():
     for index, fileName in enumerate(img_list):
         im = Image.open(fileName)
         # Discard black and white images, breaks rest of pipeline
         if (im.mode == 'RGB'):
             # If its taller than it is wide, crop first
-            #  print("File {} has dimensions {}".format(fileName,im.size))
             if (im.size[1] > im.size[0]):
                 crop_shift = random.randrange(im.size[1] - im.size[0])
                 im = im.crop((0, crop_shift, im.size[0], im.size[0] + crop_shift))
@@ -74,19 +83,16 @@ for img_type, img_list in files_dict.items():
                     training_mean_old = numpy_image
             # Save the image to the HDF5 output file
             hdf5_output[img_type][index, ...] = numpy_image
-            if index % 1000 == 0:
-                print("Saved {} {}s to hdf5 file".format(index,img_type))
+            if index % 1000 == 0 and index > 0:
+                small_end = time()
+                print("Saved {} {}s to hdf5 file in {} seconds".format(index,img_type, small_end - small_start))
+                small_start = time()
 
 # Calculate the std using the variance array
 training_std = np.zeros((*SIZE,3), dtype=np.float32)
 np.sqrt(training_variance/(len(files_dict["train_img"]) - 1), training_std)
 hdf5_output["train_mean"][...] = training_mean_new
 hdf5_output["train_std"][...] = training_std
-
-print("Mean")
-print(training_mean_new[100:120,100:120,0])
-print("STD")
-print(training_std[100:120,100:120,0])
 
 end_time = time()
 print("Elapsed time: {} seconds".format(end_time - start_time))
